34c2b92ce4846f511e73fd49819a5c0b1380879d
Mohd Feroz Baig <feroz.baig96@gmail.com>
2017-01-11 18:29:34 +0530

    added gallery-videos

--- docs/js/app.js
+++ docs/js/app.js
@@ -22,9 +22,19 @@
 
 })(jQuery, document, window);
 
+//smooth scroll for contact us
 $('#contact_us_menu').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
     return false;
 });
+
+//making height=width on first launch
+$('.family-image').height($('.family-image').width());
+
+//making height=width on window resize
+window.addEventListener('resize', function(){
+	$('.family-image').height($('.family-image').width());
+});
+
